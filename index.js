59f617c6a120f7ee18e25a54062b9e9d5287f881
Yu Hao Shao <yuhaoshao@YudeMacBook-Air.local>
2021-04-01 19:22:52 -0700

    port edited

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3000 || process.env.PORT;
 const bodyParser = require("body-parser");
 const cookieParser = require('cookie-parser');
 const bcrypt = require("bcrypt");
